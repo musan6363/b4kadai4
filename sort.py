86ca64c1a4313086e41e05c1de2bdcf054d6b42f
musan6363 <msn151336+prog@gmail.com>
2021-04-09 01:00:21 +0900

    Implemented Selection Sort and Improved procedure

--- sort.py
+++ sort.py
@@ -1,22 +1,6 @@
 import DoublyLinkedList as dll
 
 
-def swap(a, b):
-    # - a - x - x - x - b - を - b - x - x - x - a - につなぎ替える
-    # 8つの情報を更新する．
-    tmp_a_prev = a.prev
-    tmp_a_next = a.next
-    a.prev.next = b
-    a.next.prev = b
-    a.prev = b.prev
-    a.next = b.next
-    b.prev.next = a
-    b.next.prev = a
-    b.prev = tmp_a_prev
-    b.next = tmp_a_next
-    return b.prev, b, b.next, a.prev, a, a.next
-
-
 def insertion_sort(data):
     """
     >>> data1 = dll.DoublyLinkedList()
@@ -52,22 +36,16 @@ def insertion_sort(data):
             sorted = target.prev
             while sorted is not data.head and sorted.x > target.x:
                 sorted = sorted.prev
-            # ここから 挿入処理
-            # tmp = target.next
-            # target.prev.next = target.next
-            # target.next.prev = target.prev
-            # target.next = sorted.next
-            # target.prev = sorted
-            # sorted.next.prev = target
-            # sorted.next = target
-            # target = tmp
-            (
-                sorted, sorted.next, sorted.next.next,
-                target.prev, target, target.next
-            ) = swap(
-                sorted.next, target
-            )
+            new_target = target.next  # 次のtargetはtargetの次．上書きされないように保持
+            # ここから 挿入処理 sortedの後ろにtargetを挿入
+            target.prev.next = target.next
+            target.next.prev = target.prev
+            target.next = sorted.next
+            target.prev = sorted
+            sorted.next.prev = target
+            sorted.next = target
             # ここまで 挿入処理
+            target = new_target
             data.show()
     else:
         print(data)
@@ -103,43 +81,29 @@ def bubble_sort(data):
     if type(data) is dll.DoublyLinkedList:
         count = 0
         flag = True
-        unsorted_position = 0
         unsorted_top = data.head.next
         while flag:
             flag = False
-            target_position = 0
             target = data.tail.prev
             while target is not unsorted_top:
-                if target.x < target.prev.x:
+                if target.prev.x > target.x:
+                    unsorted_top_prev = unsorted_top.prev  # 上書き防止
                     # ここから 入替処理
-                    # tmp_prev = target.prev.prev
-                    # tmp_next = target.prev
-                    # target.prev.prev.next = target
-                    # target.prev.prev = target
-                    # target.prev.next = target.next
-                    # target.next.prev = target.prev
-                    # target.prev = tmp_prev
-                    # target.next = tmp_next
-                    (
-                        target.prev.prev, target.prev, target,
-                        target.prev, target, target.next
-                    ) = swap(
-                        target.prev, target
-                    )
+                    tmp_prev = target.prev.prev
+                    tmp_next = target.prev
+                    target.prev.prev.next = target
+                    target.prev.prev = target
+                    target.prev.next = target.next
+                    target.next.prev = target.prev
+                    target.prev = tmp_prev
+                    target.next = tmp_next
                     # ここまで 入替処理
+                    unsorted_top = unsorted_top_prev.next
                     flag = True
                     count += 1
-                target_position += 1
-                target = data.tail.prev
-                for i in range(target_position):
+                else:
                     target = target.prev
-                unsorted_top = data.head.next
-                for i in range(unsorted_position):
-                    unsorted_top = unsorted_top.next
-            unsorted_position += 1
-            unsorted_top = data.head.next
-            for i in range(unsorted_position):
-                unsorted_top = unsorted_top.next
+            unsorted_top = unsorted_top.next
         data.show()
         print(count)
     else:
@@ -162,21 +126,74 @@ def bubble_sort(data):
 
 
 def selection_sort(data):
+    """
+    >>> data1 = dll.DoublyLinkedList()
+    >>> data1.insert(3)
+    >>> data1.insert(1)
+    >>> data1.insert(2)
+    >>> data1.insert(4)
+    >>> data1.insert(6)
+    >>> data1.insert(5)
+    >>> selection_sort(data1)
+    selection sort
+    1 2 3 4 5 6
+    4
+    >>> data2 = [5,6,4,2,1,3]
+    >>> selection_sort(data2)
+    selection sort
+    [1, 2, 3, 4, 5, 6]
+    4
+    """
     print("selection sort")
     if type(data) is dll.DoublyLinkedList:
+        count = 0
         unsorted_top = data.head.next
-        unsorted_position = 0
         while unsorted_top is not data.tail:
+            count_flag = False
             min = unsorted_top
             target = unsorted_top.next
             while target is not data.tail:
                 if target.x < min.x:
                     min = target
+                    count_flag = True
                 target = target.next
-            # minとunsorted_topの交換
-
+            # ここから minとunsorted_topの交換
+            tmp_prev = min.prev
+            tmp_next = min.next
+            unsorted_top.prev.next = min
+            unsorted_top.next.prev = min
+            min.prev = unsorted_top.prev
+            min.next = unsorted_top.next
+            tmp_prev.next = unsorted_top
+            tmp_next.prev = unsorted_top
+            unsorted_top.prev = tmp_prev
+            unsorted_top.next = tmp_next
+            if min.next is min:
+                # minとunsorted_topが隣り合っていたときの対策
+                min.next = unsorted_top
+                unsorted_top.prev = min
+            # ここまで minとunsorted_topの交換
+            unsorted_top = min.next
+            if count_flag:
+                count += 1
+        data.show()
+        print(count)
     else:
-        pass
+        count = 0
+        for i in range(len(data)):
+            count_flag = False
+            minj = i
+            for j in range(i, len(data)):
+                if data[j] < data[minj]:
+                    minj = j
+                    count_flag = True
+            tmp = data[i]
+            data[i] = data[minj]
+            data[minj] = tmp
+            if count_flag:
+                count += 1
+        print(data)
+        print(count)
 
 
 if __name__ == "__main__":
